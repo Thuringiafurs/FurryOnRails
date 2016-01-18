c09c2cadacc8715f18d8788f39eac51b97d7f002
Lycian <big.ap@gmx.de>
2016-01-18 13:30:33 +0100

    add email+nick to usertable, secure_passwords to user model and ignore *.geany

--- db/migrate/20160111152513_create_users.rb
+++ db/migrate/20160111152513_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
+      t.string :email
+      t.string :nick
       t.string :first_name
       t.string :last_name
       t.string :password_digest
